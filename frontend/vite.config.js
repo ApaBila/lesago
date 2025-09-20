9bdeabfa6b86ea1d4dfaf431a9f91929f04f30d9
ApaBila <salsabila.mahdi@gmail.com>
2025-09-20 11:49:15 -0500

    mlp: init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         learning: resolve(__dirname, 'learning.html'),
         ols: resolve(__dirname, 'modules/ols/index.html'),
+        mlp: resolve(__dirname, 'modules/mlp/index.html'),
       },
     },
   },
